10e6e8c66896d6df95be28b8461e1424ae16341d
Rubin <rslukongo@student.apc.edu.ph>
2026-02-02 12:13:49 +0800

    adjusting the eroor in my images

--- home/js/images.js
+++ home/js/images.js
@@ -4,8 +4,11 @@
       manyFoods: [
         'https://www.w3schools.com/vue/img_burrito.svg',
         'https://www.w3schools.com/vue/img_salad.svg',
-        'https://www.w3schools.com/img_cake.svg',
+        'https://www.w3schools.com/vue/img_cake.svg',
         'https://www.w3schools.com/vue/img_soup.svg',
+        'https://www.w3schools.com/vue/img_fish.svg',
+        'https://www.w3schools.com/vue/img_pizza.svg',
+        'https://www.w3schools.com/vue/img_rice.svg',
       ]
     }
    }
